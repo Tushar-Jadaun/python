9a2c68ca821e83dd55b75fac6fd14f18e2923c53
Tushar-Jadaun <tusharjadaun2001485@gmail.com>
2025-10-11 09:59:59 +0530

    Merged remote and added chapter_01.py

---
+++ Data_Type_In_Python/first.py
@@ -0,0 +1,8 @@
+a=int(input("enter the number : "))
+b=int(input("enter the number : "))
+# c=int(input("enter the number : "))
+# d=int(input("enter the number : "))
+
+# print("sum of number :",a+b)
+z=(a+b)
+print("Average of ",z*2)
