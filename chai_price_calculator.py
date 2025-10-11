4b85ab1e373393d847c3a5f0f5b886e18e9fc63b
Tushar-Jadaun <tusharjadaun2001485@gmail.com>
2025-10-11 11:59:28 +0530

    Added chai_price_calculator.py file

---
+++ chai_price_calculator.py
@@ -0,0 +1,9 @@
+cup = input('please select the size of cup small // medium // big :').lower()
+if cup == 'small':
+    print(f' you select {cup} this price is 10 rupees ')
+elif cup == 'medium':
+    print(f'you select {cup} this price is 15 rupees')  
+elif cup == 'big':
+          print(f'you select {cup} this price is 20 rupees')
+else:
+    print(f'you select wrong thing')    
