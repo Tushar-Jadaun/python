6ab1eafa1603c0b5d64206dc8a6cf0655a4cd12b
Tushar-Jadaun <tusharjadaun2001485@gmail.com>
2025-10-11 09:54:14 +0530

    Added chapter_01.py file

---
+++ chapter_01.py
@@ -0,0 +1,19 @@
+print("Hello, Tushar! Python is working fine 🚀")
+
+a = 10
+b = 2.5
+c = "Python"
+d = [1, 2, 3]
+e = {"name": "Tushar"}
+f = (5, 6, 7)
+g = {8, 9, 10}
+h = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
